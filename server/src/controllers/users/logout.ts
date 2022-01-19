a0ebe3201d82408ab6bffa919ca0a4b886e60c32
Wi <widalida26@gmail.com>
2022-01-19 14:18:13 +0900

    Update : Logout

--- server/src/controllers/users/logout.ts
+++ server/src/controllers/users/logout.ts
@@ -3,7 +3,13 @@ import { Request, Response } from 'express';
 const logout = async (req: Request, res: Response) => {
   try {
     return res
-      .cookie('accessToken', '', { maxAge: 1 })
+      .cookie('accessToken', '', {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+        domain: process.env.DOMAIN,
+        maxAge: 1,
+      })
       .status(200)
       .send('successfully logout');
   } catch (err) {
