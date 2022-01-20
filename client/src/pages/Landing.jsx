a90d2d82ddaf79ed2a217055a3c108daff0c6c10
JLthePi <jlthepi@gmail.com>
2022-01-20 14:26:54 +0900

    Add : Redirection at Landing

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,9 +1,19 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Footer from '../components/Footer';
+import { FaArrowRight } from 'react-icons/fa';
 
-const Container = styled.div``;
+const FadeIn = keyframes`
+  from{
+    opacity:0;
+  }
+  to{
+    opacity:1;
+  }
+`;
+
 const SectionContainer = styled.div`
   height: calc(100vh - 3rem);
   overflow: auto;
@@ -62,6 +72,7 @@ const HeaderContainer = styled.div`
 const Header = styled.div`
   font-size: 2rem;
   margin-bottom: 2rem;
+  /* animation: ${FadeIn} 1s ease; */
   p {
     font-size: 1.75rem;
     word-wrap: break-word;
@@ -84,7 +95,6 @@ const HeaderContent = styled.div`
   }
 `;
 const TextContainer = styled.div`
-  flex: 1;
   font-size: 1rem;
   z-index: 5;
   display: flex;
@@ -92,6 +102,7 @@ const TextContainer = styled.div`
   margin: 0 2rem;
   max-width: 500px;
   user-select: none;
+  animation: ${FadeIn} 1s ease;
   @media all and (max-width: 1023px) {
     margin: 0;
     width: 100%;
@@ -134,11 +145,6 @@ const ImageContainer = styled.div`
     height: 100%;
     object-fit: scale-down;
   }
-
-  @media all and (max-width: 1023px) {
-    max-width: 400px;
-    max-height: 400px;
-  }
 `;
 const Chicken = styled.div`
   margin-top: 2rem;
@@ -160,12 +166,10 @@ const Landing = () => {
         if (secNum > 1) {
           setLastScroll(Date.now());
           setSecNum(secNum - 1);
-          console.log(lastScroll);
         }
       } else if (secNum < 6) {
         setLastScroll(Date.now());
         setSecNum(secNum + 1);
-        console.log(lastScroll);
       }
     }
   };
@@ -180,97 +184,103 @@ const Landing = () => {
   }, []);
 
   return (
-    <Container>
-      <SectionContainer ref={(el) => (secRef.current[0] = el)} onWheel={handleWheel}>
-        <Section ref={(el) => (secRef.current[1] = el)}>
-          <ImageContainer>
-            <img src="assets/images/Section1.png" />
-          </ImageContainer>
-          <HeaderContainer>
-            <Header>
-              <p>다 함께 만들어가는</p>
-              <p>문제 아카이브</p>
-              <div>SOLUTIONIST</div>
-            </Header>
-            <HeaderContent>
-              <p>쉽게 문제를 만들고,</p>
-              <p>만든 문제를 풀고,</p>
-              <p>공유해보세요.</p>
-            </HeaderContent>
-          </HeaderContainer>
-        </Section>
-        <Section ref={(el) => (secRef.current[2] = el)}>
-          <TextContainer>
-            <Subheader>
-              <div>가장 먼저!</div>
-              <p>만들어진 문제를 풀어보세요!</p>
-            </Subheader>
-            <Content>
-              <p>풀고 나서 결과를 확인해보세요.</p>
-              <p>다른 사람들의 생각도 알 수 있습니다.</p>
-            </Content>
-            <Chicken>치킨 모의고사 풀러가기 -></Chicken>
-          </TextContainer>
-          <ImageContainer>
-            <img src="assets/images/Section2.gif" />
-          </ImageContainer>
-        </Section>
-        <Section ref={(el) => (secRef.current[3] = el)}>
-          <ImageContainer>
-            <img src="assets/images/Section3.gif" />
-          </ImageContainer>
-          <TextContainer>
-            <Subheader>
-              <div>원하는 문제가 없을땐...</div>
-              <p>문제를 직접 만들어보세요!</p>
-            </Subheader>
-            <Content>
-              <p>SOLUTIONIST에서는</p>
-              <p>객관식 문제,</p>
-              <p>OX문제,</p>
-              <p>설문조사</p>
-              <p>를 만들 수 있습니다.</p>
-            </Content>
-            <Chicken>문제 만들기로 이동 -></Chicken>
-          </TextContainer>
-        </Section>
-        <Section ref={(el) => (secRef.current[4] = el)}>
-          <TextContainer>
-            <Subheader>
-              <div>다 만드셨나요?</div>
-              <p>공유해보세요.</p>
-            </Subheader>
-            <Content>
-              <p>공유된 링크에서 바로 문제를 풀 수 있어요.</p>
-              <p>모바일부터 PC까지 모든 기기에서 가능합니다.</p>
-            </Content>
-          </TextContainer>
-          <ImageContainer>
-            <img src="assets/images/Section4.gif" />
-          </ImageContainer>
-        </Section>
-        <Section ref={(el) => (secRef.current[5] = el)}>
-          <ImageContainer>
-            <img src="assets/images/Section5.gif" />
-          </ImageContainer>
-          <TextContainer>
-            <Subheader>
-              <p>모두 다 함께</p>
-              <p>문제를 만들수 있어요.</p>
-            </Subheader>
-            <Content>
-              <p>이미 작성 되어있는 문제 모음에</p>
-              <p>문제를 추가하거나,</p>
-              <p>오타를 수정할 수 있어요.</p>
-            </Content>
-            <Chicken>다른 사람들의 문제 보러가기 -></Chicken>
-          </TextContainer>
-        </Section>
-        <Last ref={(el) => (secRef.current[6] = el)}>
-          <Footer />
-        </Last>
-      </SectionContainer>
-    </Container>
+    <SectionContainer ref={(el) => (secRef.current[0] = el)} onWheel={handleWheel}>
+      <Section ref={(el) => (secRef.current[1] = el)}>
+        <ImageContainer>
+          <img src="assets/images/Section1.png" />
+        </ImageContainer>
+        <HeaderContainer>
+          <Header>
+            <p>다 함께 만들어가는</p>
+            <p>문제 아카이브</p>
+            <div>SOLUTIONIST</div>
+          </Header>
+          <HeaderContent>
+            <p>쉽게 문제를 만들고, 풀고, 공유해보세요.</p>
+          </HeaderContent>
+        </HeaderContainer>
+      </Section>
+      <Section ref={(el) => (secRef.current[2] = el)}>
+        <TextContainer>
+          <Subheader>
+            <div>가장 먼저!</div>
+            <p>만들어진 문제를 풀어보세요!</p>
+          </Subheader>
+          <Content>
+            <p>풀고 나서 결과를 확인해보세요.</p>
+            <p>다른 사람들의 생각도 알 수 있습니다.</p>
+          </Content>
+          <Link to="/make">
+            <Chicken>
+              치킨 모의고사 풀러가기 <FaArrowRight size="0.75rem" />
+            </Chicken>
+          </Link>
+        </TextContainer>
+        <ImageContainer>
+          <img src="assets/images/Section2.gif" />
+        </ImageContainer>
+      </Section>
+      <Section ref={(el) => (secRef.current[3] = el)}>
+        <ImageContainer>
+          <img src="assets/images/Section3.gif" />
+        </ImageContainer>
+        <TextContainer>
+          <Subheader>
+            <div>원하는 문제가 없을땐...</div>
+            <p>문제를 만들어보세요!</p>
+          </Subheader>
+          <Content>
+            <p>SOLUTIONIST에서는</p>
+            <p>객관식과 OX 문제,</p>
+            <p>설문조사를 만들 수 있습니다.</p>
+          </Content>
+          <Link to="/make">
+            <Chicken>
+              문제 만들기로 이동 <FaArrowRight size="0.75rem" />
+            </Chicken>
+          </Link>
+        </TextContainer>
+      </Section>
+      <Section ref={(el) => (secRef.current[4] = el)}>
+        <TextContainer>
+          <Subheader>
+            <div>다 만드셨나요?</div>
+            <p>공유해보세요.</p>
+          </Subheader>
+          <Content>
+            <p>공유된 링크에서 바로 문제를 풀 수 있어요.</p>
+            <p>모바일부터 PC까지 모든 기기에서 가능합니다.</p>
+          </Content>
+        </TextContainer>
+        <ImageContainer>
+          <img src="assets/images/Section4.gif" />
+        </ImageContainer>
+      </Section>
+      <Section ref={(el) => (secRef.current[5] = el)}>
+        <ImageContainer>
+          <img src="assets/images/Section5.gif" />
+        </ImageContainer>
+        <TextContainer>
+          <Subheader>
+            <p>모두 다 함께</p>
+            <p>문제를 만들수 있어요.</p>
+          </Subheader>
+          <Content>
+            <p>이미 작성 되어있는 문제 모음에</p>
+            <p>문제를 추가하거나,</p>
+            <p>오타를 수정할 수 있어요.</p>
+          </Content>
+          <Link to="/solve">
+            <Chicken>
+              다른 사람들의 문제 보러가기 <FaArrowRight size="0.75rem" />
+            </Chicken>
+          </Link>
+        </TextContainer>
+      </Section>
+      <Last ref={(el) => (secRef.current[6] = el)}>
+        <Footer />
+      </Last>
+    </SectionContainer>
   );
 };
 
